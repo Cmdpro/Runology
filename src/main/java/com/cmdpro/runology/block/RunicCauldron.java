e4be5e825aff270965706d801566d3e1cfcbff49
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2024-03-10 22:58:18 -0400

    a

--- src/main/java/com/cmdpro/runology/block/RunicCauldron.java
+++ src/main/java/com/cmdpro/runology/block/RunicCauldron.java
@@ -72,6 +72,6 @@ public class RunicCauldron extends BaseEntityBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
         return createTickerHelper(pBlockEntityType, BlockEntityInit.RUNICCAULDRON.get(),
-                RunicAnalyzerBlockEntity::tick);
+                RunicCauldronBlockEntity::tick);
     }
 }
