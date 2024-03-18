f6a3552d6943b26f0e0a5ffde2a694af831b462b
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2024-03-17 21:47:08 -0400

    a

--- src/main/java/com/cmdpro/runology/commands/RunologyCommands.java
+++ src/main/java/com/cmdpro/runology/commands/RunologyCommands.java
@@ -1,11 +1,16 @@
 package com.cmdpro.runology.commands;
 
 import com.cmdpro.runology.Runology;
+import com.cmdpro.runology.api.RunologyUtil;
 import com.cmdpro.runology.entity.RunicOverseer;
 import com.cmdpro.runology.init.EntityInit;
 import com.cmdpro.runology.moddata.ChunkModData;
 import com.cmdpro.runology.moddata.ChunkModDataProvider;
 import com.cmdpro.runology.moddata.PlayerModDataProvider;
+import com.klikli_dev.modonomicon.Modonomicon;
+import com.klikli_dev.modonomicon.book.Book;
+import com.klikli_dev.modonomicon.book.BookCategory;
+import com.klikli_dev.modonomicon.book.BookEntry;
 import com.klikli_dev.modonomicon.bookstate.BookUnlockStateManager;
 import com.klikli_dev.modonomicon.bookstate.BookVisualStateManager;
 import com.klikli_dev.modonomicon.command.ResetBookUnlocksCommand;
@@ -21,6 +26,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.chunk.LevelChunk;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class RunologyCommands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher){
@@ -65,5 +73,25 @@ public class RunologyCommands {
             });
         }
         return Command.SINGLE_SUCCESS;
-    }
+    }/*
+    private static int learnall(CommandContext<CommandSourceStack> command){
+        if(command.getSource().getEntity() instanceof Player) {
+            Player player = (Player) command.getSource().getEntity();
+            player.getCapability(PlayerModDataProvider.PLAYER_MODDATA).ifPresent(data -> {
+                data.setBookDiscoverProcess(2);
+                data.getUnlocked().clear();
+                for (Map.Entry<ResourceLocation, BookCategory> i : BookDataManager.get().getBook(new ResourceLocation(Runology.MOD_ID, "runologyguide")).getCategories().entrySet()) {
+                    ArrayList<ResourceLocation> list = new ArrayList<>();
+                    for (Map.Entry<ResourceLocation, BookEntry> o : i.getValue().getEntries().entrySet()) {
+                        if (RunologyUtil.getAnalyzeCondition(o.getValue().getCondition()) != null) {
+                            list.add(o.getKey());
+                        }
+                    }
+                    data.getUnlocked().put(i.getKey(), list);
+                }
+                BookUnlockStateManager.get().updateAndSyncFor((ServerPlayer)player);
+            });
+        }
+        return Command.SINGLE_SUCCESS;
+    }*/
 }
