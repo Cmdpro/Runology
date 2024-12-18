cb22c787727258cc33dbe1b9f0f4fe58a5c2047f
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2024-12-17 20:26:44 -0500

    shatters render properly now

--- src/main/java/com/cmdpro/runology/renderers/entity/ShatterRenderer.java
+++ src/main/java/com/cmdpro/runology/renderers/entity/ShatterRenderer.java
@@ -3,6 +3,7 @@ package com.cmdpro.runology.renderers.entity;
 import com.cmdpro.runology.entity.Shatter;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -10,12 +11,11 @@ import net.minecraft.client.renderer.blockentity.TheEndPortalRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
+import org.joml.*;
 import org.joml.Math;
-import org.joml.Matrix4f;
-import org.joml.Random;
-import org.joml.Vector3f;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +35,7 @@ public class ShatterRenderer extends EntityRenderer<Shatter> {
         super(pContext);
         Random rand = new Random();
         for (int i = 0; i < 10; i++) {
-            rotOffsets.add(new SpikeData(new Vec3((rand.nextFloat()*6f)-3f, (rand.nextFloat()*6f)-3f, (rand.nextFloat()*6f)-3f), (rand.nextFloat()*0.3f)+0.2f));
+            rotOffsets.add(new SpikeData(new Vec3((rand.nextFloat()*6f)-3f, (rand.nextFloat()*6f)-3f, (rand.nextFloat()*6f)-3f), (rand.nextFloat()*0.15f)+0.1f));
         }
     }
     public ResourceLocation getTextureLocation(Shatter pEntity) {
@@ -45,19 +45,24 @@ public class ShatterRenderer extends EntityRenderer<Shatter> {
     @Override
     public void render(Shatter entity, float entityYaw, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
         super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
-        float rotatyThing = Math.toRadians(Minecraft.getInstance().level.getGameTime() + partialTick)/2.5f;
+        float rotatyThing = Math.toRadians(Minecraft.getInstance().level.getGameTime() + partialTick)*10f;
         int ind = 0;
         for (SpikeData i : rotOffsets) {
             float xMult = (float)i.rotOffset.x;
             float yMult = (float)i.rotOffset.y;
             float zMult = (float)i.rotOffset.z;
-            float size = i.size+(Math.sin(rotatyThing+(ind*3))*0.1f);
-            Vector3f end = new Vector3f(0, 0.5f, 0).rotateX(Math.sin(rotatyThing*xMult)).rotateY(rotatyThing * yMult).rotateZ(rotatyThing * zMult);
-            renderSpike(new Vec3(0, 0.5, 0), new Vec3(end.x, end.y+0.5f, end.z), size, partialTick, poseStack, bufferSource, packedLight);
+            float size = i.size+(Math.sin((rotatyThing/20f)+(ind*3))*0.05f);
+            poseStack.pushPose();
+            poseStack.translate(0, 0.5, 0);
+            poseStack.mulPose(Axis.XP.rotation((rotatyThing * xMult) * Mth.DEG_TO_RAD));
+            poseStack.mulPose(Axis.YP.rotation((rotatyThing * yMult) * Mth.DEG_TO_RAD));
+            poseStack.mulPose(Axis.ZP.rotation((rotatyThing * zMult) * Mth.DEG_TO_RAD));
+            renderSpike(new Vec3(0, 0, 0), new Vec3(0, (i.size-(Math.sin((rotatyThing/30f)+(ind*3))*0.05f))*3f, 0), size, partialTick, poseStack, bufferSource, packedLight);
+            poseStack.popPose();
             ind++;
         }
     }
-    public void renderSpike(Vec3 start, Vec3 end, float thickness, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
+    private void renderSpike(Vec3 start, Vec3 end, float thickness, float partialTick, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight) {
         Vector3f diff = end.toVector3f().sub(start.toVector3f()).normalize().mul(thickness, thickness, thickness);
         Vector3f diffRotated = new Vector3f(diff).rotateX(Math.toRadians(90));
         VertexConsumer vertexConsumer = bufferSource.getBuffer(RenderType.endPortal());
