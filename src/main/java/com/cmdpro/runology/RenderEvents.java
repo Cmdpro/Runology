de46e1534e76cc218b61ed83f4c16cbf0c3f03a6
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2025-05-20 05:11:40 -0400

    small tweaks

--- src/main/java/com/cmdpro/runology/RenderEvents.java
+++ src/main/java/com/cmdpro/runology/RenderEvents.java
@@ -1,9 +1,9 @@
 package com.cmdpro.runology;
 
+import com.cmdpro.databank.mixin.client.BufferSourceMixin;
 import com.cmdpro.databank.multiblock.MultiblockRenderer;
 import com.cmdpro.databank.rendering.RenderHandler;
 import com.cmdpro.databank.rendering.ShaderHelper;
-import com.cmdpro.runology.mixin.client.BufferSourceMixin;
 import com.cmdpro.runology.registry.AttachmentTypeRegistry;
 import com.cmdpro.runology.shaders.RunologyRenderTypes;
 import com.mojang.blaze3d.pipeline.MainTarget;
